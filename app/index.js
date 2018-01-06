d31461d2cf10baace6374ba9adb5adb6423bb577
Lcurvelo27 <lcurvelo27@gmail.com>
2018-01-06 14:02:02 -0700

    built first component using ES5 functions

---
+++ app/index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+var App = require('./components/app')
+var Popular = require('./components/popular')
+
+
+ReactDOM.render(
+  <App  />,
+  document.getElementById('app')
+)
